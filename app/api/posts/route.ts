3ae17614f9dc200b910207a977dec1eb943f1a9f
Devon Daviau <devondaviau@yahoo.ca>
2026-07-24 14:37:35 -0400

    start building out route handler in posts/route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,1 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
 // route for new post creation using the POST method
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const params = request.nextUrl.searchParams;
+  const viewerId = params.get("viewrId");
+
+  if (!viewerId) {
+    return NextResponse.json(
+      { message: "viewerId is required" },
+      { status: 400 },
+    );
+  }
+
+  return NextResponse.json({ message: "Post Creation Route" }, { status: 200 });
+}
