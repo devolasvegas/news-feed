a5613fd836771ba022cb4b3260b078f6ea4f83e3
Devon Daviau <devondaviau@yahoo.ca>
2026-07-30 14:45:50 -0400

    create createPost function in postService

--- app/_server/services/postService.ts
+++ app/_server/services/postService.ts
@@ -1,4 +1,4 @@
-import { posts } from "../db/posts";
+import { posts, reactionCounts } from "../db/posts";
 import {
   getMediaByIds,
   getAuthorOrThrow,
@@ -8,11 +8,18 @@ import {
   type MediaDTO,
   type PostDTO,
 } from "../dto";
+import { DbPost, PostBody } from "../types";
 
 export type PostDetail = {
   post: PostDTO;
   author: AuthorDTO;
   media: MediaDTO[];
+};
+
+export type CreatePostParams = {
+  authorId: string;
+  body: PostBody;
+  mediaIds?: string[];
 };
 
 export async function getPostById(
@@ -34,6 +41,36 @@ export async function getPostById(
   return {
     post: toPostDTO(post, viewerId),
     author: toAuthorDTO(author, viewerId),
+    media: postMedia,
+  };
+}
+
+export async function createPost({
+  authorId,
+  body,
+  mediaIds,
+}: CreatePostParams): Promise<PostDetail> {
+  const author = await getAuthorOrThrow(authorId);
+  const newPost: DbPost = {
+    id: crypto.randomUUID(),
+    authorId,
+    body,
+    mediaIds: mediaIds ?? [],
+    reactionCounts: reactionCounts({}),
+    commentCount: 0,
+    shareCount: 0,
+    createdAt: Date.now(),
+  };
+
+  posts.push(newPost);
+
+  const postMedia = await getMediaByIds(newPost.mediaIds);
+  const authorDto = toAuthorDTO(author, authorId);
+  const postDto = toPostDTO(newPost, authorId);
+
+  return {
+    post: postDto,
+    author: authorDto,
     media: postMedia,
   };
 }
